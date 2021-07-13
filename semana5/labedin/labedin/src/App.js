aa3c0fdc9611217435dad47af32ed06cac9caff7
Rafael Sônego <rasonego@gmail.com>
2021-07-13 19:04:26 -0300

    Desafio + Entrega Aula 18 - Componentes Funcionais, props e styled-components

--- semana5/labedin/labedin/src/App.js
+++ semana5/labedin/labedin/src/App.js
@@ -2,50 +2,88 @@ import React from 'react';
 import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
 import ImagemButton from './components/ImagemButton/ImagemButton';
+import perfil from './img/perfil.jpg'
+import dev from './img/dev.webp'
+import react from './img/react.png'
+import email from './img/email.png'
+import location from './img/location.png'
+import servidor from './img/servidor.png'
+import escola from './img/escola.png'
+import CardPequeno from './components/CardPequeno/CardPequeno'
 
 function App() {
   return (
     <div className="App">
       <div className="page-section-container">
         <h2>Dados pessoais</h2>
-        <CardGrande 
-          imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png" 
-          nome="Astrodev" 
-          descricao="Oi, eu sou o Astrodev. Sou o chefe dos alunos da Labenu. Adoro pedir e-mails na sexta-feira e esperar os alunos responderem só para responder com uma bronca e dar mais trabalho para eles."
+        <CardGrande
+          imagem={perfil}
+          nome="Rafael"
+          descricao="Oi, eu sou o Rafael. Sou aluno da Labenu."
         />
-        
-        <ImagemButton 
-          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+
+        <ImagemButton
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png"
           texto="Ver mais"
         />
       </div>
 
       <div className="page-section-container">
-        <h2>Experiências profissionais</h2>
-        <CardGrande 
-          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
-          nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+        <CardPequeno
+          imagem={email}
+          nome="Email"
+          descricao="rasonego@google.com"
         />
-        
-        <CardGrande 
-          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
-          nome="NASA" 
-          descricao="Apontando defeitos." 
+      </div>
+      <div className="page-section-container">
+        <CardPequeno
+          imagem={location}
+          nome="Endereço"
+          descricao="Rua De Colores"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande
+          imagem={dev}
+          nome="Labenu"
+          descricao="Formando em Ciências da Computação"
+        />
+
+        <CardGrande
+          imagem={react}
+          nome="DEV"
+          descricao="Desenvolvedor Sênior - React"
+        />
+
+        <CardGrande
+          imagem={servidor}
+          nome="Infraestrutura"
+          descricao="Tech Leader - Data Center - VIVO"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências Acadêmica</h2>
+        <CardGrande
+          imagem={escola}
+          nome="Escola Dominicana"
+          descricao="Desenvolvimento do site da escola"
         />
       </div>
 
       <div className="page-section-container">
         <h2>Minhas redes sociais</h2>
-        <ImagemButton 
-          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
-          texto="Facebook" 
-        />        
+        <ImagemButton
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png"
+          texto="Facebook"
+        />
 
-        <ImagemButton 
-          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
-          texto="Twitter" 
-        />        
+        <ImagemButton
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png"
+          texto="Twitter"
+        />
       </div>
     </div>
   );
---
+++ semana5/labedin/labedin/src/components/CardPequeno/Style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Pequeno = styled.div`
+  
+  display: flex;
+  align-items: center;
+  border: 1px solid black;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+  height: 100px;
+
+
+img {
+  margin-right: 50px;
+  margin-left: 15px;
+}
+
+div {
+  display: flex;
+  flex-direction: column;
+  justify-items: flex-start;
+  
+    h4 {
+    margin-bottom: 15px;
+  }
+}
+`
